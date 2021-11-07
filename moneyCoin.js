778b23df62fe1149eecda849ca5c18af84d5f266
desembritakrnwt <93499494+desembritakrnwt@users.noreply.github.com>
2021-11-07 13:09:51 +0700

    menambahkan file moneyCoin.js

--- moneyCoin.js
+++ moneyCoin.js
@@ -12,3 +12,24 @@ console.log(moneyCoins(7752))
 
 console.log(moneyCoins(37454))
 //output : [ 10000, 10000, 10000, 5000, 2000, 200, 200, 50, 1, 1, 1, 1 ]
+
+function moneyCoins(amount, array) {
+
+  var results = [],
+     result = [],
+     total = 0;
+
+  if (array.length === 0) {
+     return results;
+  }
+
+  if (amount <= 0) {
+     return 'Enter an amount';
+  }
+
+  for (var i = 0; i < array.length; i++) {
+
+     return coin(amount, array.slice(1));
+  }
+
+}
